7aef4d05958aac73fe3c73f7326ce1c90f88bc1a
hanhongbin <hanhb1994924@qq.com>
2019-12-11 15:19:34 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+   <VoiceRecognize/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VoiceRecognize from "./components/VoiceRecognize"
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   VoiceRecognize
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/VoiceRecognize.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>voice recognize</div>
+</template>
+<script>
+export default {
+  name: "VoiceRecognize"
+};
+</script>
